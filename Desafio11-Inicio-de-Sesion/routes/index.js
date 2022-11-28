350c44e7d2d6170b12765a8764a922f30ad07a81
Gabriel Cañadas <gcanadas@unsj.edu.ar>
2022-11-28 01:30:56 -0300

    Desafio 11 - Inicio de Sesión

--- Desafio11-Inicio-de-Sesion/routes/index.js
+++ Desafio11-Inicio-de-Sesion/routes/index.js
@@ -1,45 +1,88 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   try{
-    if(req.session.user){
-      const user = req.session.user;
-      res.render('index', { title: 'Desafío 10: Log-in por Formulario', userName: user });
+    if (!req.isAuthenticated()) {
+          res.render('login');
     } else {
-      res.redirect('/login');
+          const { user } = req;
+          res.render('index', { title: 'Desafío 11: Inicio de Sesión', userName: user.email });
     }
-  } catch(error){
+  } catch (error){
     next(error);
   }
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login');
-})
-
-router.post('/login', function(req,res,next) {
-  try{
-    const user = req.body.user;
-    console.log(req.body.user)
-    req.session.user = user;
-    res.redirect('/');
-  } catch(error) {
-    next(error);
+router.post('/login', passport.authenticate('sign-in', {
+        successRedirect: '/',
+        failureRedirect: '/errorLogin',
+  }),
+  (req, res) => {
+        res.redirect('/');
   }
-})
+);
 
-router.get('/logout', function(req,res,next) {
-  const user = req.session.user;
-  req.session.destroy((error) => {
+router.get('/errorLogin', (req, res) => {
+  res.render('errorLogin', { title: 'USER ERROR LOGIN', message: 'CREDENCIALES NO VALIDAS', href: '/'});
+});
+
+router.post('/logout', (req, res, next) => {
+  const { userName } = req.body;
+  req.logout((error) => {
     if (!error) {
-      res.render('logout', { name: user });
+      res.render('logout', { email: userName });
     } else {
-      res.send('Ocurrio un error', error.message);
+      res.send('Ocurrio un  error', error.message);
     }
   });
-})
+});
 
+router.get('/register', (req, res, next) => {
+  res.render('register');
+});
+
+router.post('/register', passport.authenticate('sign-up', {
+        successRedirect: '/',
+        failureRedirect: '/errorRegister',
+  }),
+  (req, res) => {
+    const { user } = req;
+    res.json({ message: `User ${user.email} was registered.` })
+  }
+);
+
+router.get('/errorRegister', (req, res) => {
+  res.render('errorLogin', { title: 'USER ERROR REGISTER', message: 'USUARIO YA REGISTRADO', href: '/register'});
+});
+
+// router.post('/login', function(req,res,next) {
+//   try{
+//     const user = req.body.user;
+//     console.log(req.body.user)
+//     req.session.user = user;
+//     res.redirect('/');
+//   } catch(error) {
+//     next(error);
+//   }
+// })
+
+// router.get('/logout', function(req,res,next) {
+//   const user = req.session.user;
+//   req.session.destroy((error) => {
+//     if (!error) {
+//       res.render('logout', { name: user });
+//     } else {
+//       res.send('Ocurrio un error', error.message);
+//     }
+//   });
+// })
+
+// router.get('/login', function(req, res, next) {
+//   res.render('login');
+// })
 
 module.exports = router;
